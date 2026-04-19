3397609bdc99546f93dbfa5842aa37fa9bf2c26e
Petr Novák <xlamagrap@seznam.cz>
2026-04-19 21:21:49 +0200

    Fix upload: allow overwriting existing blobs

--- api/upload.js
+++ api/upload.js
@@ -24,6 +24,7 @@ export default async function handler(req, res) {
       access: 'public',
       contentType: contentType || 'image/jpeg',
       addRandomSuffix: false,
+      allowOverwrite: true,
     })
 
     return res.status(200).json({ url: blob.url })
